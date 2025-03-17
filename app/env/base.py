0ae036de1104481c933b2fa308541a0b59e0e733
Lornalone <marlyca79@gmail.com>
2025-03-17 17:08:06 +0100

    chore: update static files path 2

--- app/env/base.py
+++ app/env/base.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
@@ -112,10 +113,10 @@ MEDIA_URL = "/media/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'app' / 'static'
 ]
 
 # Default primary key field type
