a8508692eccbe34c3f27110b13905c28e86170d1
Lornalone <marlyca79@gmail.com>
2025-03-17 12:42:52 +0100

    adding staticroot in base.py

--- app/env/base.py
+++ app/env/base.py
@@ -104,13 +104,19 @@ USE_I18N = True
 USE_TZ = True
 
 
+# Media
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
