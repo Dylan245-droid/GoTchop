8668415daac44206350311f550abf1f9101c8b16
Lornalone <marlyca79@gmail.com>
2025-03-17 10:52:16 +0100

    configurate app

---
+++ app/env/prod.py
@@ -0,0 +1,22 @@
+from .base import *
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': '5432',
+    }
+}
+
+MEDIA_URL = '/eatwhere/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv("CLOUD_NAME"),
+    'API_KEY': os.getenv("API_KEY"),
+    'API_SECRET': os.getenv("API_SECRET"),
+    
+}
